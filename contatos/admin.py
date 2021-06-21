94666341704a57e495a6433a99e18094927d6859
Andre M. de Souza <andresouza44@gmail.com>
2021-06-20 21:25:23 -0300

    Agenda

--- contatos/admin.py
+++ contatos/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import Categoria,Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome','sobrenome','telefone','email')
+    list_display = ('id','nome','sobrenome','telefone','email','mostar',)
     list_display_links = ('id','nome','sobrenome')
-    list_filter = ('nome','sobrenome','categoria')
+   # list_filter = ('nome','sobrenome','categoria')
     list_per_page = (10)
     search_fields = ('nome','sobrenome','telefone')
+    list_editable = ('telefone', 'mostar')
 
 
 admin.site.register(Categoria)
--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-#from django.http import Http404
+from django.http import Http404
 from .models import  Contato
+from django.core.paginator import Paginator
+
 def index (request):
-    contatos = Contato.objects.all()
+    #contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter (mostar = True)
+    paginator = Paginator(contatos,3)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+
+
     return render (request, 'contatos/index.html',{'contatos':contatos})
 
 def ver_contato(request,contato_id):
     # contato = Contato.objects.get(id = contato_id)
     contato =   get_object_or_404(Contato,  id = contato_id)
+    if not contato.mostar:
+        raise  Http404()
     return render(request,'contatos/ver_contato.html', {'contato':contato})
